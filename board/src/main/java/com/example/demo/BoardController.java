486eb790a4e6ef2b0ec71140ab6b631c4c121893
Yelin Choe <inmoment9319@gmail.com>
2026-01-14 16:52:02 +0900

    Update BoardController.java

--- board/src/main/java/com/example/demo/BoardController.java
+++ board/src/main/java/com/example/demo/BoardController.java
@@ -9,7 +9,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/boards")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*") // 프론트엔드 연결 허용
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
 public class BoardController {
 
     private final BoardRepository boardRepository;
